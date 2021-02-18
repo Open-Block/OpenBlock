e81882be94131b3ee39744ee440778f6094c5040
mosemister <nekronmoseley@gmail.com>
2021-02-18 17:12:52 +0000

    Shows tab on enter

--- src/main/java/org/block/panel/common/navigation/TabbedNavigationBar.java
+++ src/main/java/org/block/panel/common/navigation/TabbedNavigationBar.java
@@ -1,14 +1,21 @@
 package org.block.panel.common.navigation;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.scene.input.MouseEvent;
 
 public class TabbedNavigationBar extends TabPane {
 
     public TabbedNavigationBar() {
+        this(new Tab[0]);
     }
 
     public TabbedNavigationBar(Tab... tabs) {
         super(tabs);
+        this.addEventHandler(MouseEvent.MOUSE_EXITED, e -> TabbedNavigationBar.this.getSelectionModel().getSelectedItem().getContent().setVisible(false));
+        this.addEventHandler(MouseEvent.MOUSE_ENTERED, e -> TabbedNavigationBar.this.getSelectionModel().getSelectedItem().getContent().setVisible(true));
+
     }
 }
