0fbcd23e1294e9cbc3c9a2d57ab8e08e9da0514f
mosemister <nekronmoseley@gmail.com>
2021-02-24 09:00:21 +0000

    I will get android running

--- src/main/java/org/temp/FXProjectPanelsAndroid.java
+++ src/main/java/org/temp/FXProjectPanelsAndroid.java
@@ -1,7 +1,11 @@
 package org.temp;
 
 import com.gluonhq.charm.glisten.application.MobileApplication;
+import com.gluonhq.charm.glisten.control.TextArea;
 import com.gluonhq.charm.glisten.mvc.View;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import org.array.utils.ArrayUtils;
 import org.block.Blocks;
 import org.block.MobileBlocks;
 import org.block.panel.launch.ProjectsPanel;
@@ -15,9 +19,14 @@ public class FXProjectPanelsAndroid extends MobileApplication {
     public void init() {
         ShinyOutputStream.createDefault();
         this.addViewFactory(HOME_VIEW, () -> {
-          View view = new View(new ProjectsPanel(new File("Projects")));
-        Blocks.setInstance(new MobileBlocks(view));
-          return view;
+            try {
+                View view = new View(new ProjectsPanel(new File("Projects")));
+                Blocks.setInstance(new MobileBlocks(view));
+                return view;
+            } catch (Throwable e) {
+                String stackTrace = ArrayUtils.toString("\n\t", t -> t.getFileName() + "[" + t.getLineNumber() + "]", e.getStackTrace());
+                return new View(new VBox(new Label(e.getMessage()), new TextArea(stackTrace)));
+            }
         });
     }
 
